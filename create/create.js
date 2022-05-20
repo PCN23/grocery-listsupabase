1d4ed2619a34edb4794dd49b51210a1aeae9472d
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-19 17:55:59 -0700

    adding create js file

--- create/create.js
+++ create/create.js
@@ -1,24 +1,62 @@
-import { checkAuth, createListItem, logout } from '../fetch-utils.js';
+import { checkAuth, createListItem, getListItems, logout } from '../fetch-utils.js';
 
 checkAuth();
 
 const error = document.getElementById('error');
+const form = document.getElementById('food-form');
+const deleteButton = document.querySelector('.delete');
+const listEl = document.querySelector('.list');
+
+checkAuth();
+
 const logoutButton = document.getElementById('logout');
 
+deleteButton.addEventListener('click', async () => {
+    await deleteAllListItems();
+
+    await fetchAndDisplayList();
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
-const form = document.getElementById('food-form');
+window.addEventListener('load', async () => {
+    await fetchAndDisplayList();
+});
+
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const itemData = new FormData(form);
-    const data = await createListItem(itemData.get('name'), itemData.get('quantity'));
+    const Data = new FormData(form);
+    const item = await createListItem(Data.get('name'), Data.get('quantity'));
     if (data) {
         window.location.href = '/list';
     } else {
         error.textContent = 'something went wrong';
     }
+    await fetchAndDisplayList();
+
 });
 
+async function fetchAndDisplayList() {
+    const list = await getListItems();
+
+    listEl.textContent = '';
+    for (let item of list) {
+        const listItemEl = document.createElement('p');
+
+        listItemEl.classList.add('food-form');
+        listItemEl.textContent = `${item.name} ${item.quantity}`;
+
+        if (item.bought) {
+            listItemEl.classList.add('bought');
+        } else {
+            listItemEl.classList.add('not-bought');
+            listItemEl.addEventListener('click', async () => {
+                await buyListItem(item.id);
+                fetchAndDisplayList();
+            });
+        }
+        listEl.append(listItemEl);
+    }
+}
